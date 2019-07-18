1ea361190c92e9eecc9cf91955c44e5cd837341c
Laura <laurakcleve@gmail.com>
2019-07-18 13:56:07 -0600

    Display inventory items in view

--- client/src/components/Inventory.jsx
+++ client/src/components/Inventory.jsx
@@ -1,12 +1,45 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Query } from 'react-apollo'
+import { gql } from 'apollo-boost'
+
+const INVENTORY_ITEMS_QUERY = gql`
+  query inventoryItems {
+    inventoryItems {
+      id
+      item {
+        id
+        name
+      }
+      add_date
+      expiration
+      amount
+    }
+  }
+`
 
 const Inventory = () => {
   return (
-    <div>
-      <h1>Inventory</h1>
-      <Link to="/">Home</Link>
-    </div>
+    <Query query={INVENTORY_ITEMS_QUERY}>
+      {({ data, loading, error }) => {
+        if (loading) return <p>Loading...</p>
+        if (error) return <p>Error</p>
+
+        return (
+          <div>
+            <h1>Inventory</h1>
+            <Link to="/">Home</Link>
+            <ul>
+              {data.inventoryItems.map((inventoryItem) => (
+                <li>
+                  <span>{inventoryItem.item.name}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )
+      }}
+    </Query>
   )
 }
 
